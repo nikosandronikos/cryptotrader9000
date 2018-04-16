edd0dfba039d325c18aaa334215c7b5a0cc33229
Nikos Andronikos <dodgeyhack@gmail.com>
2018-04-16 15:38:15 +1000

    Add bare config

---
+++ src/config.mjs
@@ -0,0 +1,16 @@
+export {config};
+
+const config = {
+    timeout: 3000,
+    coinList: [],
+    accounts: [{
+      name: 'name',
+      key: 'key',
+      secret: 'secret'}
+    ],
+    telegram: {
+        key: 'key',
+        channel: 'channel'
+    }
+};
+
