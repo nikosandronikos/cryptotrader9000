a053f6c965e31eb8e1e75801d663b0d69cbf4ed8
Nikos Andronikos <dodgeyhack@gmail.com>
2018-05-14 16:42:47 +1000

    Support differing levels of verbosity for logging

--- src/log.mjs
+++ src/log.mjs
@@ -3,10 +3,26 @@ import {TelegramBot} from './telegram.mjs';
 const Console = console;
 
 /**
+ * Options for differing verbosity of log output.
+ */
+export const LogLevelType = {
+    none:   0,
+    error:  1,
+    notify: 2,
+    info:   4,
+    debug:  8
+}
+
+/**
  * @access package
  */
 class Log {
     constructor() {
+        /**
+         * Set to a value from {@link LogLevel} to set the verbosity of output.
+         */
+        this.level = LogLevelType.debug;
+
         if (process.env.TELEGRAM_KEY !== undefined
             &&
             process.env.TELEGRAM_CHANNEL !== undefined
@@ -20,20 +36,24 @@ class Log {
     }
 
     error(...args) {
+        if (this.level < LogLevelType.error) return;
         if (this.telegram) this.telegram.message(args.join(' '));
         Console.log(...args);
     }
 
     notify(...args) {
+        if (this.level < LogLevelType.notify) return;
         if (this.telegram) this.telegram.message(args.join(' '));
         Console.log(...args);
     }
 
     info(...args) {
+        if (this.level < LogLevelType.info) return;
         Console.log(...args);
     }
 
     debug(...args) {
+        if (this.level < LogLevelType.debug) return;
         Console.log(...args);
     }
 }
